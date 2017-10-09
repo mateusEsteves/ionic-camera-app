58d101c143dc58915ae2e51634347bcda6074bfc
Mateus <mateus.esteves1994@gmail.com>
2017-10-09 17:21:21 -0300

    mudanças

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,12 +11,10 @@ export class HomePage {
 
   img_base64: any;
 
-  
-
   constructor(public navCtrl: NavController, public foto: FotoProvider) {
     console.log('HomeComponent Loaded');
   }
-
+  
   public addPicture(): void {
     this.foto.takePicture().then((data) => {
       this.img_base64 = data.img_str;
